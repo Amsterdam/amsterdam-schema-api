64065802a07a6a8c67ee2c34a486a34b60ad40ec
Jona Bosman <j.bosman@amsterdam.nl>
2025-09-18 17:17:41 +0200

    Inintial setup tests

--- src/tests/test_views.py
+++ src/tests/test_views.py
@@ -1,6 +1,23 @@
+import pytest
+from django.urls import reverse
+
+
 def test_root_view(client):
     response = client.get("/status/")
     assert response.status_code == 200
-    assert response.json() == {
-        "status": "online"
-    }
+    assert response.json() == {"status": "online"}
+
+
+@pytest.mark.django_db
+def test_dataset_listview(client):
+    response = client.get(reverse("dataset-list"))
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_dataset_detailview(client):
+    response = client.get(
+        reverse("dataset-detail"),
+        kwargs={"name": "bomen"},
+    )
+    assert response.status_code == 200
