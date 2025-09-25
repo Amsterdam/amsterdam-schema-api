0228a3dad0b7d621a5dd04c4f43234e0194224fc
Jona Bosman <j.bosman@amsterdam.nl>
2025-09-25 17:39:43 +0200

    Endpoints for scopes and publishers

--- src/schema_api/views.py
+++ src/schema_api/views.py
@@ -4,9 +4,10 @@ from django.views import View
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from schematools.contrib.django.models import Dataset
+from schematools.contrib.django.models import Dataset, Publisher, Scope
+from schematools.exceptions import DatasetVersionNotFound
 
-from .utils import filter_on_scope, simplify_json
+from .utils import simplify_json
 
 
 class RootView(View):
@@ -17,7 +18,7 @@ class RootView(View):
 
 
 class DatasetViewSet(viewsets.ReadOnlyModelViewSet):
-    lookup_field = "name"
+    lookup_field = "id"
 
     def get_queryset(self):
         return Dataset.objects.all()
@@ -34,25 +35,68 @@ class DatasetViewSet(viewsets.ReadOnlyModelViewSet):
         json_queryset = [simplify_json(dataset.schema) for dataset in queryset]
         return Response(json_queryset)
 
-    def retrieve(self, request, name):
+    def retrieve(self, request, id):
         datasets = self.get_queryset()
-        dataset = get_object_or_404(datasets, name=name)
+        dataset = get_object_or_404(datasets, name=id)
 
         return Response(dataset.schema)
 
     @action(detail=True, url_path=r"(?P<vmajor>\w+)")
-    def version(self, request, name, vmajor):
+    def version(self, request, id, vmajor):
         datasets = self.get_queryset()
-        dataset = get_object_or_404(datasets, name=name)
+        dataset = get_object_or_404(datasets, name=id)
         try:
             dataset_vmajor = dataset.schema.get_version(vmajor)
-        except Exception as e:
-            raise e
-
-        # Filter on scope if provided in the request
-        scope = self.request.query_params.get("scope")
-        if scope:
-            scoped_schema = filter_on_scope(dataset_vmajor, scope)
-            return Response(scoped_schema)
+        except DatasetVersionNotFound as e:
+            return Response(status=404, data={"detail": str(e)})
 
         return Response(dataset_vmajor.json_data())
+
+
+class ScopeViewSet(viewsets.ReadOnlyModelViewSet):
+
+    # currently, id = TOPDESKBV/FENI
+    # / messes with url path
+    # lookup_field = "name"
+
+    def get_queryset(self):
+        return Scope.objects.all()
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            json_queryset = [scope.schema for scope in page]
+            return self.get_paginated_response(json_queryset)
+
+        json_queryset = [dataset.schema for dataset in queryset]
+        return Response(json_queryset)
+
+    def retrieve(self, request, pk):
+        scopes = self.get_queryset()
+        scope = get_object_or_404(scopes, pk=pk)
+
+        return Response(scope.schema)
+
+
+class PublisherViewSet(viewsets.ReadOnlyModelViewSet):
+    lookup_field = "id"
+
+    def get_queryset(self):
+        return Publisher.objects.all()
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            json_queryset = [publisher.schema for publisher in page]
+            return self.get_paginated_response(json_queryset)
+
+        json_queryset = [dataset.schema for dataset in queryset]
+        return Response(json_queryset)
+
+    def retrieve(self, request, id):
+        publishers = self.get_queryset()
+        publisher = get_object_or_404(publishers, name=id)
+
+        return Response(publisher.schema)
