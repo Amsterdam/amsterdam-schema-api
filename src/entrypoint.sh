585b18281ea4fe6a66fe114e0a24ecd2309200b2
JonaBenja <j.bosman@amsterdam.nl>
2025-10-08 12:57:19 +0200

    Move management commands to Dockerfile entrypoint

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -6,4 +6,4 @@ python manage.py import_schemas --execute --no-migrate-tables
 python manage.py import_scopes
 python manage.py import_publishers
 python manage.py import_profiles
-uwsgi --py-auto-reload=1 --enable-threads --lazy-apps --buffer-size=65535
+uwsgi
