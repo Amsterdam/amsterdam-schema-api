bf83995264ed9fe0802bd705332ac108a3cb27ef
yrob <y.robert@amsterdam.nl>
2025-12-24 12:26:57 +0100

    Warm-up Django during WSGI startup

--- src/schema_api/wsgi.py
+++ src/schema_api/wsgi.py
@@ -5,6 +5,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 """
 
 import os
+import sys
 
 from django.conf import settings
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +15,19 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "schema_api.settings")
 
 application = get_wsgi_application()
 application = WhiteNoise(application, root=settings.STATIC_ROOT)
+
+# Warm-up Django ahead of time instead of lazy-apps
+# From: http://uwsgi-docs.readthedocs.org/en/latest/articles/TheArtOfGracefulReloading.html#dealing-with-ultra-lazy-apps-like-django
+application(
+    {
+        "REQUEST_METHOD": "GET",
+        "SERVER_NAME": "127.0.0.1",
+        "SERVER_PORT": "80",
+        "PATH_INFO": "/v1/",
+        "wsgi.input": sys.stdin,
+        "wsgi.url_scheme": "http",
+        "wsgi.errors": sys.stderr,
+        "wsgi.version": (1, 0),
+    },
+    lambda x, y: None,
+)
