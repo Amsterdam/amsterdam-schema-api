217b7272964aaee252727106ee0e40041d74d991
JonaBenja <j.bosman@amsterdam.nl>
2026-01-08 16:08:39 +0100

    Reuse checkout out commits+ temp dir is automatically deleted

--- src/schema_api/scripts/checkout_update_commit.sh
+++ src/schema_api/scripts/checkout_update_commit.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 set -e
 
-# Load the 2 commits to compare from the command line
-base_commit=$1
-update_commit=$2
-tmp_dir=$3
+# Load update commit and tmp folder
+update_commit=$1
+tmp_dir=$2
 
-# Checkout and save the repo for the 2 commits
+# Checkout and save the repo for the update commit
 cd $tmp_dir/amsterdam-schema
-mkdir ../changes/${base_commit}
 mkdir ../changes/${update_commit}
 
-git archive "$base_commit" | tar -x -C "../changes/${base_commit}"
 git archive "$update_commit" | tar -x -C "../changes/${update_commit}"
 
 # Return the date on which the update commit was merged (format-local means in timezone of current user)
